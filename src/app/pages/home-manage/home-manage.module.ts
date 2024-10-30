81602e9010c46c22f73f6aa941746d6b9dad6ab1
VanityEmperor <2765812425@qq.com>
2024-10-30 21:30:23 +0800

    home-manage模块建立

---
+++ src/app/pages/home-manage/home-manage.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeManageComponent } from './home-manage.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HomeManageComponent]
+})
+export class HomeManageModule { }
