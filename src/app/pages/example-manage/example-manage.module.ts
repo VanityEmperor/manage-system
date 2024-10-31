cbe1b07329053469ee8f56ce34dd0e76ff35256c
VanityEmperor <2765812425@qq.com>
2024-10-31 21:53:15 +0800

    添加example-one组件

--- src/app/pages/example-manage/example-manage.module.ts
+++ src/app/pages/example-manage/example-manage.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AntduiModule } from '../../antdui/antdui/antdui.module';
+import { ExampleOneComponent } from './example-one/example-one.component';
+import { ExampleManageRoutes } from './example-manage.routing';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AntduiModule,
+    ExampleManageRoutes
   ],
-  declarations: []
+  declarations: [ExampleOneComponent]
 })
 export class ExampleManageModule { }
--- src/app/pages/example-manage/example-manage.routing.ts
+++ src/app/pages/example-manage/example-manage.routing.ts
@@ -1,7 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
-
+import { ExampleOneComponent } from './example-one/example-one.component';
 const routes: Routes = [
-  {  },
+  { 
+    path: 'exampleOne', 
+    component: ExampleOneComponent,
+    data: {
+      breadcrumb: '首页展示'
+    }
+   },
 ];
 
 export const ExampleManageRoutes = RouterModule.forChild(routes);
